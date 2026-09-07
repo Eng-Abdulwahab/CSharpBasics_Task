142dbeef64ffc0344e3182ba7a995fd70505d296
Abdulwahab <abdulwahab.r.software@gmail.com>
2026-09-07 22:49:26 +0300

    Organize project structure

---
+++ src/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace CSharpBasics_Task
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
---
+++ src/TaskMethodsLibrary.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpBasics_Task
+{
+    internal class TaskMethodsLibrary
+    {
+
+    }
+}
